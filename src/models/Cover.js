9a6b2bbd764baa2b0cb7217fdfd5945e742fb5b7
cgranda <Cesar.Granda@baruch.cuny.edu>
2023-07-14 14:13:46 -0400

    first committ

---
+++ src/models/Cover.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const mongoosePaginate = require('mongoose-paginate-v2');
+
+const CoverSchema = new Schema(
+    {
+        coverName: {
+            type: String,
+            required: true,
+        },
+        letter: {
+            type: String,
+            required: true,
+        },
+
+    },
+    { timestamps: { currentTime: () => Date.now() } },
+);
+
+CoverSchema.plugin(mongoosePaginate)
+
+
+
+const Cover = mongoose.model('cover', CoverSchema);
+module.exports = Cover;
